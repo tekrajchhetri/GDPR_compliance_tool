b9ad2527109bf62ad24576867aa794ee19a1e4a3
Tek Raj Chhetri <52251022+tekrajchhetri@users.noreply.github.com>
2021-05-09 20:13:13 +0200

    Token generator

---
+++ core/Token.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 08.05.21 11:56
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @File    : Token.py
+# @Software: PyCharm
+
+import datetime
+import hashlib
+
+class TokenGenerator:
+    def token_generator(self):
+        return hashlib.sha512(str(datetime.datetime.timestamp(datetime.datetime.now())).encode("utf-8")).hexdigest()
