c9b125b0b231b7078fb706793a0c9bb085fd5353
Tek Raj Chhetri <52251022+tekrajchhetri@users.noreply.github.com>
2021-05-12 19:21:07 +0200

    refactoring

---
+++ core/Credentials.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 12.05.21 15:24
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @File    : Credentials.py
+# @Software: PyCharm
+import os
+
+class Credentials:
+    def get_username(self):
+        return os.environ.get("USERNAME")
+
+    def get_password(self):
+        return os.environ.get("PASSWORD")
