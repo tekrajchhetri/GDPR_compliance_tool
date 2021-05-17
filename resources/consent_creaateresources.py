d5adcacc9d08882093ecfab578df88a616e89dfa
Tek Raj Chhetri <52251022+tekrajchhetri@users.noreply.github.com>
2021-05-17 22:56:53 +0200

    consent based on BC1

--- resources/consent_creaateresources.py
+++ resources/consent_creaateresources.py
@@ -14,50 +14,57 @@ from core.QueryEngine import QueryEngine
 import json
 
 
-class ConsentRequest(Schema):
-    ConsentIDInput = fields.String(required=True, description="Consent ID")
-    DataControllerInput = fields.String(required=True, description="Data Controller")
-    DataInput = fields.String(required=True, description="What data? EG. GPS")
-    DataProcessingInput = fields.String(required=True, description="What Purpose? EG. Analysis")
-    DataRequesterInput = fields.String(required=True, description="Who want data? EG. LexisNexis")
-    DurationInput = fields.String(required=True, description="How many days? EG. 10days")
-    GrantedAtTimeInput = fields.DateTime(required=True, description="Time consent given")
-    MediumInput = fields.String(required=True, description="What medium consent is provided by?")
-    NameInput = fields.String(required=True, description="Data subject")
-    PurposeInput = fields.String(required=True, description="For what purpose")
+class ConsentRequests(Schema):
+    PersonaldataId = fields.String(required=True,
+                                   description="Unique personal identifier")
+    applicationMappings = fields.Dict(required=False, keys=fields.Str(), values=fields.Str())
+    GrantedAtTime = fields.DateTime(required=True, description="Date when consent was provided")
+    GivenAtLocation = fields.String(required=True, description="Location from where consent was given")
+    ExpirationDate = fields.DateTime(required=True, description="Consent expiry date")
+    Resource = fields.Dict(required=True, keys=fields.Str(), values=fields.List(fields.Str()))
+    dataUsage = fields.List(fields.String(),required=True,
+                            description='["GPS","speed","milleage","personalData","trafficJamLocation"]')
+    Medium = fields.String(required=True, description="How consent was given? Eg. Online, MobileApp")
+    Agents = fields.List(fields.String(),required=True,
+                         description='["60a55c53d79bc757698041e9","60a55c9dd79bc757698041ea"]')
+    Purpose = fields.String(required=True, description="For what purpose")
+    DataProcessing = fields.List(fields.String(),required=True,
+                                 description='["Analysis, collection, storage"]')
+
 
 class ConsentCreate(MethodResource, Resource):
     @doc(description='create consent.', tags=['Create Consent'])
-    @use_kwargs(ConsentRequest)
+    @use_kwargs(ConsentRequests)
+    @marshal_with(ConsentRequests)
     def post(self, **kwargs):
-        query = QueryEngine()
-        parser = reqparse.RequestParser()
-        parser.add_argument('ConsentIDInput', required=True,  location="json")
-        parser.add_argument('DataControllerInput', required=True, location="json")
-        parser.add_argument('DataInput', required=True,location="json")
-        parser.add_argument('DataProcessingInput', required=True, location="json")
-        parser.add_argument('DataRequesterInput', required=True, location="json")
-        parser.add_argument('DurationInput', required=True, location="json")
-        parser.add_argument('GrantedAtTimeInput', required=True,location="json")
-        parser.add_argument('MediumInput', required=True,  location="json")
-        parser.add_argument('NameInput', required=True,location="json")
-        parser.add_argument('PurposeInput', required=True, location="json")
-        # data = request.get_json(force=True)
-        args = parser.parse_args()
-        response = query.post_data(ConsentIDInput = args["ConsentIDInput"],
-                                   DataControllerInput = args["DataControllerInput"] ,
-                                   DataInput = args["DataInput"] ,
-                                   DataProcessingInput = args["DataProcessingInput"] ,
-                                   DataRequesterInput = args["DataRequesterInput"],
-                                   DurationInput = args["DurationInput"],
-                                   GrantedAtTimeInput = args["GrantedAtTimeInput"] ,
-                                   MediumInput = args["MediumInput"],
-                                   NameInput = args["NameInput"],
-                                   PurposeInput = args["PurposeInput"])
+        print(kwargs["PersonaldataId"])
+        # query = QueryEngine()
+        # parser = reqparse.RequestParser()
+        # parser.add_argument('ConsentIDInput', required=True,  location="json")
+        # parser.add_argument('DataControllerInput', required=True, location="json")
+        # parser.add_argument('DataInput', required=True,location="json")
+        # parser.add_argument('DataProcessingInput', required=True, location="json")
+        # parser.add_argument('DataRequesterInput', required=True, location="json")
+        # parser.add_argument('DurationInput', required=True, location="json")
+        # parser.add_argument('GrantedAtTimeInput', required=True,location="json")
+        # parser.add_argument('MediumInput', required=True,  location="json")
+        # parser.add_argument('NameInput', required=True,location="json")
+        # parser.add_argument('PurposeInput', required=True, location="json")
+        # # data = request.get_json(force=True)
+        # args = parser.parse_args()
+        # response = query.post_data(ConsentIDInput = args["ConsentIDInput"],
+        #                            DataControllerInput = args["DataControllerInput"] ,
+        #                            DataInput = args["DataInput"] ,
+        #                            DataProcessingInput = args["DataProcessingInput"] ,
+        #                            DataRequesterInput = args["DataRequesterInput"],
+        #                            DurationInput = args["DurationInput"],
+        #                            GrantedAtTimeInput = args["GrantedAtTimeInput"] ,
+        #                            MediumInput = args["MediumInput"],
+        #                            NameInput = args["NameInput"],
+        #                            PurposeInput = args["PurposeInput"])
 
 
-        return response
-
+        return kwargs
 
 class QueryAllConsentID(MethodResource, Resource):
     @doc(description='Get all consent ID.', tags=['All ConsentID'])
