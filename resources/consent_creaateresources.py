7314aaf2b930bcacb8d895a7f4390fd8896b7efa
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2021-05-21 22:28:56 +0200

    finalised consent structure based on BC1

--- resources/consent_creaateresources.py
+++ resources/consent_creaateresources.py
@@ -11,22 +11,36 @@ from flask_apispec.views import MethodResource
 from marshmallow import Schema, fields
 from flask_apispec import marshal_with, doc, use_kwargs
 from core.QueryEngine import QueryEngine
+import datetime
 import json
 
 
+class ForNestedSchema(Schema):
+    data = fields.List(fields.String())
+    name = fields.Str()
+
+class AgentsSchema(Schema):
+    id = fields.Str()
+    role = fields.Str()
+
 class ConsentRequests(Schema):
     PersonaldataId = fields.String(required=True,
-                                   description="Unique personal identifier")
-    applicationMappings = fields.Dict(required=False, keys=fields.Str(), values=fields.Str())
-    GrantedAtTime = fields.DateTime(required=True, description="Date when consent was provided")
+                                   default_value= "AX13RCSD442",
+                                   )
+    GrantedAtTime = fields.DateTime(required=True,
+                                    description="Date when consent was provided")
     GivenAtLocation = fields.String(required=True, description="Location from where consent was given")
-    ExpirationDate = fields.DateTime(required=True, description="Consent expiry date")
-    Resource = fields.Dict(required=True, keys=fields.Str(), values=fields.List(fields.Str()))
-    dataUsage = fields.List(fields.String(),required=True,
+    ExpirationDate = fields.DateTime(required=True,
+                                     description="Consent expiry date")
+    Resource = fields.Dict(
+        required=True, keys=fields.Str(),
+        values=fields.List(fields.Nested(ForNestedSchema))
+    )
+    dataUsage = fields.List(fields.Str(),
+                            required=True,
                             description='["GPS","speed","milleage","personalData","trafficJamLocation"]')
     Medium = fields.String(required=True, description="How consent was given? Eg. Online, MobileApp")
-    Agents = fields.List(fields.String(),required=True,
-                         description='["60a55c53d79bc757698041e9","60a55c9dd79bc757698041ea"]')
+    Agents = fields.List(fields.Nested(AgentsSchema),required=True)
     Purpose = fields.String(required=True, description="For what purpose")
     DataProcessing = fields.List(fields.String(),required=True,
                                  description='["Analysis, collection, storage"]')
@@ -35,36 +49,11 @@ class ConsentRequests(Schema):
 class ConsentCreate(MethodResource, Resource):
     @doc(description='create consent.', tags=['Create Consent'])
     @use_kwargs(ConsentRequests)
-    @marshal_with(ConsentRequests)
     def post(self, **kwargs):
-        print(kwargs["PersonaldataId"])
-        # query = QueryEngine()
-        # parser = reqparse.RequestParser()
-        # parser.add_argument('ConsentIDInput', required=True,  location="json")
-        # parser.add_argument('DataControllerInput', required=True, location="json")
-        # parser.add_argument('DataInput', required=True,location="json")
-        # parser.add_argument('DataProcessingInput', required=True, location="json")
-        # parser.add_argument('DataRequesterInput', required=True, location="json")
-        # parser.add_argument('DurationInput', required=True, location="json")
-        # parser.add_argument('GrantedAtTimeInput', required=True,location="json")
-        # parser.add_argument('MediumInput', required=True,  location="json")
-        # parser.add_argument('NameInput', required=True,location="json")
-        # parser.add_argument('PurposeInput', required=True, location="json")
-        # # data = request.get_json(force=True)
-        # args = parser.parse_args()
-        # response = query.post_data(ConsentIDInput = args["ConsentIDInput"],
-        #                            DataControllerInput = args["DataControllerInput"] ,
-        #                            DataInput = args["DataInput"] ,
-        #                            DataProcessingInput = args["DataProcessingInput"] ,
-        #                            DataRequesterInput = args["DataRequesterInput"],
-        #                            DurationInput = args["DurationInput"],
-        #                            GrantedAtTimeInput = args["GrantedAtTimeInput"] ,
-        #                            MediumInput = args["MediumInput"],
-        #                            NameInput = args["NameInput"],
-        #                            PurposeInput = args["PurposeInput"])
-
-
-        return kwargs
+        schema = ConsentRequests()
+        data = request.get_json(force=True)
+        validated_data = schema.load(data)
+        return validated_data
 
 class QueryAllConsentID(MethodResource, Resource):
     @doc(description='Get all consent ID.', tags=['All ConsentID'])
