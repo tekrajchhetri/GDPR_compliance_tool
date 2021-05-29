532354ff7ad60b02a1fe828885f4e3a215204d00
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2021-05-29 21:19:12 +0200

    init faas query function

---
+++ core/func/query/handler.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 29.05.21 21:14
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @File    : handler.py
+# @Software: PyCharm
+
+def handle(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+
+    return "to be implemented"
