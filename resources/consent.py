5cf59efb355dfc798e231c7e99ea56a27462b472
Tek Raj Chhetri <52251022+tekrajchhetri@users.noreply.github.com>
2021-06-04 11:52:24 +0200

    swagger response added

--- resources/consent.py
+++ resources/consent.py
@@ -8,7 +8,7 @@
 
 from flask_restful import Resource, reqparse, request
 from flask_apispec.views import MethodResource
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields,ValidationError
 from flask_apispec import marshal_with, doc, use_kwargs
 from core.QueryEngine import QueryEngine
 import datetime
@@ -45,15 +45,35 @@ class ConsentRequests(Schema):
     DataProcessing = fields.List(fields.String(),required=True,
                                  description='["Analysis, collection, storage"]')
 
+class ReturnSchema(Schema):
+    # decisionToken = fields.String(required=True)
+    act_status_code = fields.Integer(required=True)
+    decision = fields.String(required=True)
+    decision_token = fields.String(required=True)
+    message = fields.String(optional=True)
+    timestamp = fields.DateTime(required=True)
+
 
 class ConsentCreate(MethodResource, Resource):
     @doc(description='create consent.', tags=['Create Consent'])
     @use_kwargs(ConsentRequests)
+    @marshal_with(ReturnSchema, code=200)
     def post(self, **kwargs):
-        schema = ConsentRequests()
+        schema_serializer = ConsentRequests()
         data = request.get_json(force=True)
-        validated_data = schema.load(data)
-        return validated_data
+        try:
+            validated_data = schema_serializer.load(data)
+        except ValidationError as e:
+            return {"error": str(e)}, 400
+
+        qe = QueryEngine()
+        response = qe.post_data(
+            #add data
+
+        )
+
+
+        return response
 
 class QueryAllConsentID(MethodResource, Resource):
     @doc(description='Get all consent ID.', tags=['All ConsentID'])
@@ -61,6 +81,7 @@ class QueryAllConsentID(MethodResource, Resource):
         query = QueryEngine()
         return json.loads(query.select_query_gdb(consentProvidedBy=None, purpose=None, dataProcessing=None,
                                                  dataController=None, dataRequester=None, additionalData="bconsentID"))
+
 
 
 
