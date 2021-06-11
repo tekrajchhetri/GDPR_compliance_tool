5bfcd14e1824761037da911f00acfdbce1cd319c
Amar Tauqeer <amar.tauqeer@gmail.com>
2021-06-11 14:15:05 +0200

    docker is configured

--- ContractAPIs/app.py
+++ ContractAPIs/app.py
@@ -25,7 +25,7 @@ app.config.update({
         openapi_version='2.0.0'
     ),
     'APISPEC_SWAGGER_UI_URL': '/swagger-ui/',
-    
+
 })
 # contract api end points with swagger documentation
 docs = FlaskApiSpec(app)
