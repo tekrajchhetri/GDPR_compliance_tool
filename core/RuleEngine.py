c7e6de885df69165693b3303d40d775b909c412b
Tek Raj Chhetri <52251022+tekrajchhetri@users.noreply.github.com>
2021-06-21 10:26:57 +0200

    init rule engine

---
+++ core/RuleEngine.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 21.06.21 10:26
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @File    : RuleEngine.py
+# @Software: PyCharm
+
+class RuleEngine:
+    def __init__(self):
+        pass
