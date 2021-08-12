3abd9c4f8252bba0b8384b385c7a934f05f6304a
Tek Raj Chhetri <52251022+tekrajchhetri@users.noreply.github.com>
2021-08-12 15:42:43 +0200

    OpenFaaS query features updated

--- core/storage/Functions.py
+++ core/storage/Functions.py
@@ -10,12 +10,23 @@ import requests
 class Functions:
     def __init__(self):
         self.STORE = "https://openfaas.sti2.at/function/store"
+        self.QUERY = "https://openfaas.sti2.at/function/query"
+
 
     def store(self, data):
         header = {"Content-Type": "application/json"} 
         response = requests.post(self.STORE,
                             headers=header,
                             json=data)
+        return response.text
+
+    def invoke_query_function(self, json_data):
+        header = {"Content-Type": "application/json"}
+        response = requests.get(self.QUERY,
+                                json=json_data,
+                                headers=header
+                                )
+
         return response.text
 
 
