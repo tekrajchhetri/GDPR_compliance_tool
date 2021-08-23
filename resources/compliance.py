a073b279315fe2c87352cdb2eec0e62cd2753262
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2021-08-23 18:40:06 +0200

    init compliance resources

---
+++ resources/compliance.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 23.08.21 17:45
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @Web     : http://tekrajchhetri.com/
+# @File    : compliance.py
+# @Software: PyCharm
+
+
+from flask_restful import Resource
+from core.cron.Cron import Cron
+
+
