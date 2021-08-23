20613a218473c20a4bd673256de1c423f4324a3e
Tek Raj Chhetri <52251022+tekrajchhetri@users.noreply.github.com>
2021-08-23 21:07:00 +0200

    automated compliance check init

---
+++ core/cron/Cron.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time    : 23.08.21 17:29
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @Web     : http://tekrajchhetri.com/
+# @File    : Cron.py.py
+# @Software: PyCharm
+from core.compliance.ComplianceEngine import ComplianceEngine
+
+class Cron():
+
+    def __init__(self):
+        self.ce = ComplianceEngine()
+
+    def automated_compliance(self):
+        #call list of automated compliance check
+        pass
+
+
+
+if __name__ == '__main__':
+    # perform compliance check on X intervals as set in  ofelia
+    crons = Cron()
+    crons.automated_compliance()
