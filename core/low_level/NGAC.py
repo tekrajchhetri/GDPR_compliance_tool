7eef34ff9f48dfb7f0e1b00243eb715d05b510e4
Tek Raj Chhetri <52251022+tekrajchhetri@users.noreply.github.com>
2022-11-02 11:43:56 +0100

    parallelisation added

--- core/low_level/NGAC.py
+++ core/low_level/NGAC.py
@@ -8,68 +8,103 @@
 
 import requests
 import json
+import time
+import aiohttp
+import asyncio
+
+
 class NGAC:
-    def addControllerNGAC(self, _controller_id, _privacy_policy):
+    def __init__(self) -> None:
+        self.results = []
+
+    async def gather_with_concurrency(self, n, *tasks):
+        semaphore = asyncio.Semaphore(n)
+
+        async def sem_task(task):
+            async with semaphore:
+                return await task
+
+        return await asyncio.gather(*(sem_task(task) for task in tasks))
+
+    async def addControllerNGAC(self, session, _controller_id, _privacy_policy):
         _URL = "REST_ENDPOINT"
 
-        _data={
-            "token":"admin_token",
-            "data_controller":f"dc[{_controller_id}]",
-            "privacy_policy":_privacy_policy
+        payload = {
+            "token": "admin_token",
+            "data_controller": f"dc[{_controller_id}]",
+            "privacy_policy": _privacy_policy
         }
+        async with session.post(_URL,
+                                data=payload) as resp:
+            data = await resp.text()
+            return json.loads(data)['respStatus']
 
-        _r = requests.post(_URL, data=_data)
-        print(_r.text)
-        return json.loads(_r.text)['respStatus']
-
-
-    def addProcessorNGAC(self, _processor_id, _data_controller,_privacy_policy):
+    async def addProcessorNGAC(self, session, _processor_id, _data_controller, _privacy_policy):
         _URL = "REST_ENDPOINT"
 
-        _data = {
+        payload = {
             "token": "admin_token",
             "data_processor": f"dp[{_processor_id}]",
             "data_controller": f"dc[{_data_controller}]",
             "privacy_policy": _privacy_policy
         }
+        async with session.post(_URL,
+                                data=payload) as resp:
+            data = await resp.text()
+            return json.loads(data)['respStatus']
 
-        _r = requests.post(_URL, data=_data)
-        print(_r.text)
-        return json.loads(_r.text)['respStatus']
-
-
-    def addDataSubjectNGAC(self, _data_subject_id,_privacy_preference,_data_items):
+    async def addDataSubjectNGAC(self, session, _data_subject_id, _privacy_preference, _data_items):
         _URL = "REST_ENDPOINT"
 
-        _data = {
+        payload = {
             "token": "admin_token",
             "data_subject": f"ds[{_data_subject_id}]",
             "privacy_preference": _privacy_preference,
             "data_items": _data_items
         }
 
-        _r = requests.post(_URL, data=_data)
-        print(_r.text)
-        return json.loads(_r.text)['respStatus']
-    
-    def updateNGAC(self, requestedBy,hasDataController,hasDataProcessor, fordataprocessing, GrantedAtTime, inMedium, purpose,
-                     isAboutData, city, consentID, country, state, dataprovider, expirationtime):
+        async with session.post(_URL,
+                                data=payload) as resp:
+            data = await resp.text()
+            return json.loads(data)['respStatus']
+
+    async def updateNGAC(self, requestedBy, hasDataController, hasDataProcessor, fordataprocessing, GrantedAtTime,
+                         inMedium, purpose,
+                         isAboutData, city, consentID, country, state, dataprovider, expirationtime):
 
         yourprivacypoicy = "YYY"
-        yourprivacy_preference="XXX"
-        your_data_items="XXX"
+        yourprivacy_preference = "XXX"
+        your_data_items = "XXX"
 
-        controller = self.addControllerNGAC(_controller_id=hasDataController, _privacy_policy=yourprivacypoicy)
-        processor = self.addProcessorNGAC(_processor_id=hasDataProcessor,_data_controller=hasDataController,
-                                          _privacy_policy=yourprivacypoicy)
-        datasubject = self.addDataSubjectNGAC(_data_subject_id=dataprovider,
-                                              _privacy_preference=yourprivacy_preference,
-                                              _data_items=your_data_items)
+        conn = aiohttp.TCPConnector(limit=None, ttl_dns_cache=300)
+        session = aiohttp.ClientSession(connector=conn)
 
-        if 'failure' in [controller, processor, datasubject]:
-            return "fail"
+        conc_req = 3
+        now = time.time()
+        results = await self.gather_with_concurrency(conc_req, *[
+            self.addControllerNGAC(session=session, _controller_id=hasDataController, _privacy_policy=yourprivacypoicy),
+            self.addProcessorNGAC(session=session, _processor_id=hasDataProcessor, _data_controller=hasDataController,
+                                  _privacy_policy=yourprivacypoicy),
+            self.addDataSubjectNGAC(session=session, _data_subject_id=dataprovider,
+                                    _privacy_preference=yourprivacy_preference,
+                                    _data_items=your_data_items)
+            ])
+        time_taken = time.time() - now
+
+        await session.close()
+
+        if 'failure' in results:
+            return "fail", time_taken
         else:
-            return 'success'
+            return 'success', time_taken
 
 
-        
+# if __name__ == '__main__':
+#     test = NGAC()
+#     r = asyncio.run(test.updateNGAC(hasDataProcessor="XXX", hasDataController="YYYY",
+#                                  requestedBy=None, fordataprocessing=None, GrantedAtTime=None, inMedium=None,
+#                                  purpose=None,
+#                                  isAboutData=None, city=None, consentID=None, country=None, state=None,
+#                                  dataprovider=None, expirationtime=None))
+#     print(r)
+
